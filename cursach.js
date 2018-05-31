59c8759368fc46da929a732fbaf6f49e74b312c6
Александр <alek81299@gmail.com>
2018-06-01 00:06:15 +0300

    work

--- cursach.js
+++ cursach.js
@@ -1,78 +1,83 @@
 'use strict';
 
-const identity = () => 1;
+
+const identity =  i => i;
 const RIGHT = 1;
 const LEFT = 0;
 const oppDir = dir => (dir === RIGHT ? LEFT : RIGHT);
 
 class Binarytree {
   constructor(value, identifier = identity) {
-    this.value = value;
-    this.children = [];
-    this.identifier = identifier;
-    this.parent = undefined;
+     	this.value = value;
+     	this.children = [];
+     	this.identifier = identifier;
+     	this.parent = undefined;
   }
   get right() {
-    return this.children[RIGHT];
+   	return this.children[RIGHT];
   }
   get left() {
-    return this.children[LEFT];
+   	return this.children[LEFT];
   }
   set right(value) {
-    this.children[RIGHT] = value;
+   	 this.children[RIGHT] = value;
   }
   set left(value) {
-    this.children[LEFT] = value;
+   	 this.children[LEFT] = value;
   }
   get isRightChild() {
-    return this.parent ? this.parent.right === this : false;
+   	return this.parent ? this.parent.right === this : false;
   }
   get isLeftChild() {
-    return this.parent ? this.parent.left === this : false;
+   	return this.parent ? this.parent.left === this : false;
   }
   get isRoot() {
-    return this.parent === undefined;
+   	return this.parent === undefined;
   }
-  get uncle() {
-    return this.grandparent ? (this.parent.isRightChild ? this.grandparent.left : this.grandparent.right) : undefined;
+  get  uncle() {
+   	 return this.grandparent ? (this.parent.isRightChild ?
+   	 	this.grandparent.left :
+   	 	this.grandparent.right) :
+   	    undefined;
   }
   get siding() {
-    return this.parent ? (this.isRightChild ? this.parent.left : this.parent.right) : undefined;
+    	return this.parent ? (this.isRightChild ? this.parent.left : this.parent.right) : undefined;
   }
   get grandparent() {
-    return this.parent ? this.parent.parent : undefined;
+    	return this.parent ? this.parent.parent : undefined;
   }
   get isLeaf() {
-    return this.children.every(child => child === undefined);
+    	return this.children.every(child => child === undefined);
   }
   get hasOneChild() {
     return (this.right !== undefined && this.left === undefined) ||
-           (this.right === undefined && this.left !== undefined);
+       (this.right === undefined && this.left !== undefined);
   }
   get hasTwoChildren() {
-    return (this.right !== undefined && this.left !== undefined);
+    	return (this.right !== undefined && this.left !== undefined);
   }
+
 
   _swapWithParent() {
     const replacement = new Binarytree(this.value, this.identifier);
     replacement.parent = this.parent;
     replacement.children = this.children;
     if (this.parent !== undefined) {
-      if (this.isRightChild) {
-        this.parent.right = replacement;
-      } else {
-        this.parent.left = replacement;
-      }
+    	if (this.isRightChild) {
+    		this.parent.right = replacement;
+    	} else {
+    		this.parent.left = replacement;
+    	}
     }
     this.value = replacement.parent.value;
     this.children = replacement.parent.children;
     this.parent = replacement.parent.parent;
+
     this.children.forEach(child => { if (child) child.parent = this; });
     this.children.forEach(child => {
-      if (child) child.children.forEach(kid => {
-        if (kid) kid.parent = child;
-      });
+      if (child) child.children.forEach(kid => { if (kid) kid.parent = child; });
     });
+
   }
   rotateRight() {
     this._rotate(RIGHT);
@@ -92,28 +97,53 @@ class Binarytree {
     this.children.forEach(child => { if (child) child.parent = this; });
   }
 
+
   find(value) {
     const identifiedValue = this.identifier(value);
     const thisValue = this.identifier(this.value);
     if (thisValue === identifiedValue) {
       return this.value;
     } else {
-      let dir;
-      if (thisValue < identifiedValue) {
-        dir = RIGHT;
-      } else {
-        dir = LEFT;
-      } if (this.children[dir] === undefined) {
+    	let dir;
+    	if (thisValue < identifiedValue) {
+    		dir = RIGHT;
+    	} else {
+    		dir = LEFT;
+    	} if (this.children[dir] === undefined) {
         return undefined;
-      } else {
-        return this.children[dir].find(value);
-      }
+    	} else {
+    		return this.children[dir].find(value);
+    	}
     }
   }
   contains(value) {
     return this.find(value) !== undefined;
   }
+
+  insert(value) {
+  	if (this.value === undefined) {
+  		this.value = value;
+  		return this;
+  	} else {
+  		let dir;
+  		if (this.identifier(value) > this.identifier(this.value)) {
+  			dir = RIGHT;
+  		} else {
+  			dir = LEFT;
+  		}
+  		if (this.children[dir] === undefined) {
+  			const newTree = new Binarytree(value, this.identifier);
+  			newTree.parent = this;
+  			this.children[dir] = newTree;
+  			return newTree;
+  		} else {
+  			return this.children[dir].insert(value);
+  		       }
+  	       }
+  }
+
 }
+
 
 const BLACK = 'b';
 const RED = 'r';
@@ -139,36 +169,45 @@ class RedBlackTree extends Binarytree {
     this.children = replacement.parent.children;
     this.parent = replacement.parent.parent;
     this.color = replacement.parent.color;
+
     this.children.forEach(child => { if (child) child.parent = this; });
-    this.children.forEach(child => { if (child) child.children.forEach(kid => { if (kid) kid.parent = child; }); });
+
+    this.children.forEach(child => {
+      if (child) child.children.forEach(kid => { if (kid) kid.parent = child; });
+    });
+
   }
   paintBlack() {
     this.color = BLACK;
   }
   paintRed() {
-    this.color = RED;
+	    this.color = RED;
   }
   get isBlack() {
-    return this.color === BLACK;
+    	return this.color === BLACK;
   }
   get isRed() {
-    return this.color === RED;
+    	return this.color === RED;
   }
+
+
 
   paint() {
     return this._insert1;
   }
   _insert1() {
-    if (this.parent === undefined) {
+    if (this.parent === undefind) {
       this.paintBlack();
       return;
     } else {
       this._indert2();
     }
+
+
   }
 
   _insert2() {
-    if (this.parent.IsBlack) {
+    if (this.parent.isBlack) {
       return;
     } else {
       this._insert3();
@@ -185,8 +224,8 @@ class RedBlackTree extends Binarytree {
       return;
     } else
       this._insert4();
-  }
 
+  }
   _insert4() {
     if (this.isRightChild && this.parent.isRightChild) {
       this.parent.rotateLeft();
@@ -198,7 +237,6 @@ class RedBlackTree extends Binarytree {
       this._insert5();
     }
   }
-
   _insert5() {
     this.parent.paintBlack();
     this.grandparent.paintRed();
@@ -227,7 +265,7 @@ class RedBlackTree extends Binarytree {
     } else {
       const child = new RedBlackTree(value, this.identifier, RED);
       child.parent = this;
-      // const parent = child.parent;
+      const parent = child.parent;
       this.children[dir] = child;
       child.paint();
       return child;
@@ -235,12 +273,20 @@ class RedBlackTree extends Binarytree {
   }
 }
 
+
 const rbt = new RedBlackTree();
 
-rbt.paint(1);
-rbt.paint(2);
-rbt.paint(3);
-rbt.paint(4);
-rbt.contains(4);
-console.log();
+//rbt.paint(1);
+// rbt.paint(2);
+// rbt.paint(3);
+// rbt.paint(4);
+rbt.insert(7);
 
+rbt.insert(3);
+rbt.insert(2);
+
+rbt.insert(5);
+rbt.rotateRight();
+console.log(rbt);
+console.log(' ');
+console.log(rbt.right);
